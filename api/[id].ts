47e4e2534ac426f83ce706cb6a2eee538cceecfb
Richard Harrah <topplethenunnery@gmail.com>
2020-10-12 23:40:00 -0400

    add support for posting new documents

--- api/[id].ts
+++ api/[id].ts
@@ -1,10 +1,20 @@
 import { NowRequest, NowResponse } from "@vercel/node";
 import axios from "axios";
 
+const getRawContentsFromHasteBin = async (id: string): Promise<string> => {
+    const resp = await axios.get(`https://hastebin.com/raw/${id}`);
+    return resp.data;
+};
+
+
+
 export default async (req: NowRequest, res: NowResponse) => {
     try {
-        const {data: rawContents} = await axios.get(`https://hastebin.com/raw/${req.query.id}`);
-        return res.status(200).send(rawContents);
+        if (req.method?.toLowerCase() === "get") {
+            const rawContents = await getRawContentsFromHasteBin(req.query.id.toString());
+            return res.status(200).send(rawContents);
+        }
+        return res.status(500).send(`Unsupported method: ${req.method}`);
     } catch (err) {
         return res.status(500).send(`Unable to get contents of paste from HasteBin: ${err}`)
     }
---
+++ api/index.ts
@@ -0,0 +1,21 @@
+import { NowRequest, NowResponse } from "@vercel/node";
+import axios from "axios";
+
+const postRawContentsToHasteBin = async (contents: string): Promise<string> => {
+  const resp = await axios.post("https://hastebin.com/documents", contents);
+  return `https://hastebin.com/${resp.data.code}`;
+};
+
+export default async (req: NowRequest, res: NowResponse) => {
+  try {
+    if (req.method?.toLowerCase() === "post") {
+      const rawContents = await postRawContentsToHasteBin(req.body);
+      return res.status(200).send(rawContents);
+    }
+    return res.status(500).send(`Unsupported method: ${req.method}`);
+  } catch (err) {
+    return res
+      .status(500)
+      .send(`Unable to create paste on HasteBin: ${err}`);
+  }
+};
